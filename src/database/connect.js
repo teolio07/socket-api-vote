f9bca552462b9c271a82a2fbd22a016762844b7a
Jheison Rua <ruateo555@gmail.com>
2023-03-15 17:15:18 -0500

    fix mongo

--- src/database/connect.js
+++ src/database/connect.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-const Uri = process.env.URI || 'mongodb+srv://nodejs-databases:mono19teo@cluster0.08d2x.mongodb.net/votes?retryWrites=true&w=majority';
+const Uri = process.env.URI ||  'mongodb://adminStore:mono19teo@cluster0-shard-00-00.08d2x.mongodb.net:27017,cluster0-shard-00-01.08d2x.mongodb.net:27017,cluster0-shard-00-02.08d2x.mongodb.net:27017/votes?ssl=true&replicaSet=atlas-qyi3nf-shard-0&authSource=admin&retryWrites=true&w=majority';
 if(Uri){
     mongoose.connect(Uri)
     .then((resolve)=>{
